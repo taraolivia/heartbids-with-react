95be33ff204fafeed79c92d27c7579cd5c5ecb68
taraolivia <151462360+taraolivia@users.noreply.github.com>
2025-02-05 18:44:06 +0100

    Set up vite, react and typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  content: ["./index.html", "./auth/**/*.html", "./profile/**/*.html", "./post/**/*.html", "./src/**/*.{js,ts}"],
-  theme: {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+    theme: {
     extend: {
       fontFamily: {
         heading: 'Spectral',
