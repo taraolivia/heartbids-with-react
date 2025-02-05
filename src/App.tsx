f4ee17a875d9414b8e8665a08b7931270fadd377
taraolivia <151462360+taraolivia@users.noreply.github.com>
2025-02-05 19:07:33 +0100

    Trying to get tailwind to work

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-500">Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
